14c7f4c8bf60f07f2d58cfad439e30da94ef55e4
justi <justi@localhost.localdomain>
2020-04-04 11:10:50 +0200

    Add wifi and mqtt to SDS011 example

--- sensors/SDS011/sds011_nodemcu/sds011_nodemcu.ino
+++ sensors/SDS011/sds011_nodemcu/sds011_nodemcu.ino
@@ -2,12 +2,30 @@
 
 #include "SdsDustSensor.h" // https://github.com/lewapek/sds-dust-sensors-arduino-library
 
+//#include <WiFi.h> //ESP32
+#include <ESP8266WiFi.h> //ESP8266
+#include <PubSubClient.h> //MQTT
+#include "config.h"
+#include "wifiJusti.h"
+#include "mqttJusti.h"
+
+//define sds011 value variables
+double pm2dot5  = 0.0;  // Feuchtigkeit
+double pm10   = 0.0;  // Temperatur
+
 int rxPin = D1;
 int txPin = D2;
 SdsDustSensor sds(rxPin, txPin);
 
 void setup() {
   Serial.begin(9600);
+  while (!Serial);
+
+  // connecting wifi and mqtt server
+  connectWifi();
+  Serial.println("Connecting to MQTT");
+  connectMqtt(MQTT_CLIENTID_6);
+
   sds.begin();
 
   Serial.println(sds.queryFirmwareVersion().toString()); // prints firmware version
@@ -17,15 +35,27 @@ void setup() {
 
 void loop() {
   PmResult pm = sds.readPm();
-  if (pm.isOk()) {
+  if (pm.isOk())
+  {
+
+    // save values from sds011 reading:
+    pm2dot5 = pm.pm25;
+    pm10 = pm.pm10;
+
     Serial.print("PM2.5 = ");
-    Serial.print(pm.pm25);
+    Serial.print(pm2dot5);
     Serial.print(", PM10 = ");
-    Serial.println(pm.pm10);
+    Serial.println(pm10);
 
     // if you want to just print the measured values, you can use toString() method as well
     Serial.println(pm.toString());
-  } else {
+
+    publishData(MQTT_SDS011_TOPIC_LEVEL_1, MQTT_PM2DOT5, pm2dot5);
+    publishData(MQTT_SDS011_TOPIC_LEVEL_1, MQTT_PM10, pm10);
+
+  }
+  else
+  {
     Serial.print("Could not read values from sensor, reason: ");
     Serial.println(pm.statusToString());
   }
---
+++ sensors/SDS011/sds011_nodemcu/wifiJusti.h
@@ -0,0 +1,1 @@
+/home/justi/git/SensorsAndActors/ownLibrary/wifiJusti.h
