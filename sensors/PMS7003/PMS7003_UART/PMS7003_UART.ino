95d7056231b0ca1722ea4bbedca02bddd97acb63
Fabian Justi <fabian.justi@methodpark.de>
2020-04-19 21:01:58 +0200

    PMS7003 Add correct int output

--- sensors/PMS7003/PMS7003_UART/PMS7003_UART.ino
+++ sensors/PMS7003/PMS7003_UART/PMS7003_UART.ino
@@ -20,6 +20,27 @@ int chksum=0,res=0;;
 unsigned char pms[32]={0,};
 
 
+// PROBABLY BEST NOT TO CHANGE THESE VARIABLES
+byte inputHigh = 0;
+byte inputLow = 0;
+
+uint16_t concPM1_0_CF1;                             // Lots of sensor variables
+uint16_t concPM2_5_CF1;
+uint16_t concPM10_0_CF1;
+uint16_t concPM1_0_amb;
+int concPM1_0_amb_int;
+uint16_t concPM2_5_amb;
+int concPM2_5_amb_int;
+uint16_t concPM10_0_amb;
+int concPM10_0_amb_int;
+int concPM10_0_amb_int_2;
+uint16_t rawGt0_3um;
+uint16_t rawGt0_5um;
+uint16_t rawGt1_0um;
+uint16_t rawGt2_5um;
+uint16_t rawGt5_0um;
+
+
 if(mySerial.available()>=32)
 {
 
@@ -54,13 +75,42 @@ Serial.print("PM1.0 ug/m3 Atmos. Environ: ");
 Serial.print(pms[10]);
 Serial.print(pms[11]);
 Serial.println();
+concPM1_0_amb = (pms[10] << 8) + pms[11];
+concPM1_0_amb_int = uint8_t(pms[10])<<8 | uint8_t(pms[11]);
+Serial.println(concPM1_0_amb);
+Serial.println(concPM1_0_amb_int);
+Serial.println("the last two were int");
+Serial.print("pms [10] BIN ");
+Serial.println(pms[10], BIN);
+Serial.print("pms [11] BIN ");
+Serial.println(pms[11], BIN);
 Serial.print("PM2.5 ug/m3 Atmos. Environ: ");
 Serial.print(pms[12]);
 Serial.print(pms[13]);
 Serial.println();
+concPM2_5_amb = (pms[12]<< 8) + (pms[13]);
+concPM2_5_amb_int = uint8_t(pms[12])<<8 | uint8_t(pms[13]);
+Serial.println(concPM2_5_amb);
+Serial.println(concPM2_5_amb_int);
+Serial.println("the last two were int");
+Serial.print("pms [12] BIN ");
+Serial.println(pms[12], BIN);
+Serial.print("pms [13] BIN ");
+Serial.println(pms[13], BIN);
 Serial.print("PM10  ug/m3 Atmos. Environ: ");
 Serial.print(pms[14]);
 Serial.println(pms[15]);
+concPM10_0_amb = (pms[14]<< 8)+(pms[15]);
+concPM10_0_amb_int = (pms[14]<< 8)+(pms[15]);
+concPM10_0_amb_int_2= uint8_t(pms[14])<<8 | uint8_t(pms[15]);
+Serial.println(concPM10_0_amb);
+Serial.println(concPM10_0_amb_int);
+Serial.println(concPM10_0_amb_int_2);
+Serial.println("the last three were int");
+Serial.print("pms [14] BIN ");
+Serial.println(pms[14], BIN);
+Serial.print("pms [15] BIN ");
+Serial.println(pms[15], BIN);
 Serial.print("Particle beyond 0.3 um in 0.1L air: ");
 Serial.print(pms[16]);
 Serial.println(pms[17]);
