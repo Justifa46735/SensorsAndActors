36162ff3c37a1159a690a3b2958b4c917bdd793b
Fabian Justi <fabianjusti@gmx.de>
2025-03-09 00:46:56 +0100

    Add SCD41 initial example

--- ownLibrary/config.h
+++ ownLibrary/config.h
@@ -16,23 +16,22 @@ const char*   MQTT_CLIENTID_8   = "airquality-client_8";
 const char*   MQTT_CLIENTID_9   = "airquality-client_9";
 const char*   MQTT_USERNAME   = "username";
 const char*   MQTT_PASSWORD   = "password";
-const String  MQTT_BASE_TOPIC = "mp/meetingrooms/airquality";
-const String  MQTT_SUBSCRIBE_TOPIC_LEVEL_1 = "mpRoom";
 const String  MQTT_SUBSCRIBE_TOPIC_LEVEL_2 = "getData";
 
-const int     MQTT_RETRY_WAIT = 5000;
+const int     MQTT_RETRY_WAIT = 500;
 
 // Message/Datatypes of the sensor values
 // They will be also be part of the mqtt Topic
 
 const char*  MQTT_TEMPERATURE = "temperature";
+const char*  MQTT_HEATINDEX = "heatindex";
 const char*  MQTT_PRESSURE = "pressure";
 const char*  MQTT_HUMIDITY = "humidity";
 const char*  MQTT_GAS = "gas";
 const char*  MQTT_ALTITUDE = "altitude";
 const char*  MQTT_PPM = "ppm";
+const char*  MQTT_PPM_UART = "ppm_uart";
 const char*  MQTT_PPM_PWM = "ppm_pwm";
-const char*  MQTT_PM1 = "pm1";
 const char*  MQTT_PM2DOT5 = "pm2dot5";
 const char*  MQTT_PM10 = "pm10";
 
